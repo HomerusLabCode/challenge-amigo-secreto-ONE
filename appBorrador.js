f93864ed08f71a07d9df4970e429ad7f0bac4732
Liliana M. Murillo <78320736+HomerusLabCode@users.noreply.github.com>
2025-03-18 17:29:59 -0600

    Changed sript src to appBorrador.js

---
+++ appBorrador.js
@@ -0,0 +1,4 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// Deberás crear una función llamada "calculateWinner" que reciba como parámetro un array de 3 strings, cada uno representando una fila de un tablero de 3x3.
+// El tablero de 3x3 se representa de la siguiente forma:   0 1 2   3 4 5   6 7 8
+// La función deberá retornar un string con el nombre del ganador. En caso de no haber ganador, deberá retornar "Empate".
