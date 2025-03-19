e8ca134cdc52db632a8c94e3cfd360939aeab64f
Liliana M. Murillo <78320736+HomerusLabCode@users.noreply.github.com>
2025-03-18 18:11:13 -0600

    Changed image

--- appBorrador.js
+++ appBorrador.js
@@ -2,3 +2,12 @@
 // Deberás crear una función llamada "calculateWinner" que reciba como parámetro un array de 3 strings, cada uno representando una fila de un tablero de 3x3.
 // El tablero de 3x3 se representa de la siguiente forma:   0 1 2   3 4 5   6 7 8
 // La función deberá retornar un string con el nombre del ganador. En caso de no haber ganador, deberá retornar "Empate".
+// Agregar nombres a la lista de amigos cuando el usuario presione el botón "Añadir".
+// El nombre debe ser añadido a la lista de amigos y no debe ser vacío.
+// Mostrar la lista de nombres en la página.
+// Agregar un botón para cada nombre que permita eliminarlo de la lista.
+// Agregar un botón que permita eliminar todos los nombres de la lista.
+// Agregar un botón que permita cambiar el nombre de un amigo de la lista.
+// Realizar el sorteo aleatorio, asegurándose de que nadie se asigne a sí mismo.
+// Mostrar el resultado del sorteo en la página.
+// Agregar un botón que permita reiniciar el sorteo.
